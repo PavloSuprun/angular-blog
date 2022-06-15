a04a3dc6ba69b50599132b286a67d2bc18e82283
PavloSuprun <73907408+PavloSuprun@users.noreply.github.com>
2022-06-15 14:56:08 +0300

    Angular-blog app done

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,7 @@
+import {Environment} from './interface';
+
+export const environment: Environment = {
+  production: true,
+  apiKey: 'AIzaSyDa_zpCcKdw-x7gx1OWxhZPMn0xI-PJiXc',
+  fbDbUrl: 'https://angular-blog-76e19-default-rtdb.europe-west1.firebasedatabase.app/'
+};
